417b4df4b7778dd53978c89eb2e4f6a783971454
keelvin <kevinemily0523@gmail.com>
2017-07-05 10:34:04 +0800

    addSomething

---
+++ app.js
@@ -0,0 +1,14 @@
+App({
+  onLaunch: function () {
+    console.log('onLaunch')
+  },
+  onShow: function () {
+    console.log('onShow')
+  },
+  onHide: function () {
+    console.log('onHide')
+  },
+  other: function () {
+
+  }
+})
